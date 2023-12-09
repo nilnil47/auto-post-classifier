8646d2e7daa5389ea773712b4f7941b3c7e0166b
nilnil47 <nilnil47@gmail.com>
2023-12-09 11:12:37 +0200

    basic tests works

--- auto_post_classifier/test_main.py
+++ auto_post_classifier/test_main.py
@@ -17,39 +17,27 @@ def set_up_tests():
 
     response = client.post("/rank", json=request)
     assert response.status_code == 200
+    response_data = response.json()
     assert type(response.json()) == dict
-    print(response.json())
-    tests = VolunteersValidation(response.json(), volunteers_validation)
-    tests.df.to_csv("test.csv")
-    return tests
 
-class VolunteersValidation:
-    def __init__(self, response: dict, volunteers_classification: dict) -> None:
-        for uuid, response_entry in response.items():
-            response_entry["volunteers"] = volunteers_classification[uuid]
+
+    for uuid, response_entry in response_data.items():
+        response_entry["volunteers"] = volunteers_validation[uuid]
         
-        df = pd.DataFrame.from_dict(response, orient='index')
-        # todo: delete the first colum
-        self.df = df
+    df = pd.DataFrame.from_dict(response_data, orient='index')
+    # fixme: fix this
+    df.to_csv("test.csv")
+    df = pd.read_csv('test.csv')
+    return df
 
-        
-    @staticmethod
-    def classic_antisemitism(response):
-        response["antisemitism_rnk"] == 1
 
-    @staticmethod
-    def pro_palestine(response):
-        return True
-    
-    @staticmethod
-    def not_harmful(response):
-        df = pd.DataFrame(response)
-        df = df.loc[:, df.columns.str.startswith('_rnk')]
-        return True 
+df : pd.DataFrame = set_up_tests()
 
-tests : VolunteersValidation = set_up_tests()
 
 def test_validation():
+    # print(df.loc[:, "error"].isnull().all())
+    print(df["error"])
+    assert df.loc[:, "error"].isnull().all() == True
     
 # volunteers_validation_options = {
 #     "ClassicAntisemitism": lambda response: response["antisemitism_rnk"] == 1,
@@ -77,32 +65,32 @@ def test_validation():
 #     assert update_config_response.json()["test"] == "test"
 
 
-def _check_validation(response: dict):
-    assert len(response.items()) == 5
-    for uuid, post_response in response.items():
-        assert post_response["error"] == ""
-        _check_correct_astimation(uuid, post_response)
+# def _check_validation(response: dict):
+#     assert len(response.items()) == 5
+#     for uuid, post_response in response.items():
+#         assert post_response["error"] == ""
+#         _check_correct_astimation(uuid, post_response)
         
 
-def _check_correct_astimation(post_response, astimation) -> bool:
+# def _check_correct_astimation(post_response, astimation) -> bool:
     
-    pass
+#     pass
 
 
 
-def test_5_pots():
-    with open("sample.json") as f:
-        data = json.load(f)
+# def test_5_pots():
+#     with open("sample.json") as f:
+#         data = json.load(f)
 
-    request = data["request"]
-    volunteers_validation = data["wanted_responses"]
+#     request = data["request"]
+#     volunteers_validation = data["wanted_responses"]
 
-    response = client.post("/rank", json=request)
-    assert response.status_code == 200
-    assert type(response.json()) == dict
-    print(response.json())
-    tests = VolunteersValidation(response.json(), volunteers_validation)
-    tests.df.to_csv("test.csv")
+#     response = client.post("/rank", json=request)
+#     assert response.status_code == 200
+#     assert type(response.json()) == dict
+#     print(response.json())
+#     tests = VolunteersValidation(response.json(), volunteers_validation)
+#     tests.df.to_csv("test.csv")
 
     # _check_validation(response.json())
     # _check_correct_astimation()
